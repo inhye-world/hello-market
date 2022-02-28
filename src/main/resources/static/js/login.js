2cb5735bab8e24a5b863f07cae722915a080f433
inhye-world <inhye1028@gmail.com>
2022-02-28 18:22:12 +0900

    sign up css and js in progress

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,49 +1,44 @@
 'use strict';
 
+/*[ login Validate ]*/
+const inputData = document.querySelector('#loginForm');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const inputValidate = document.querySelectorAll('.wrap-input.validate-input');
+const inputArr = [username, password];
 
-    /*[ login Validate ]*/
-    const inputData = document.querySelector('#loginForm');
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
-    const inputValidate = document.querySelectorAll('.wrap-input.validate-input');
-    const inputArr = [username, password];
-
-    inputData.addEventListener('submit', (event) => {
-        /*회원가입 validation참고
-        if(checkRequred([username, password])){
-            checkPasswordMatch(password1, password2)
-        }*/
-        for(let i=0; i<inputArr.length; i++) {
-            if(inputArr[i].value.trim() === ''){
-                event.preventDefault();
-                inputValidate[i].classList.add('alert-validate');
-            }
+inputData.addEventListener('submit', (event) => {
+    for(let i=0; i<inputArr.length; i++) {
+        if(inputArr[i].value.trim() === ''){
+            event.preventDefault();
+            inputValidate[i].classList.add('alert-validate');
         }
-    });
+    }
+});
 
-    inputValidate.forEach((click) => {
-        click.addEventListener('click', (event) => {
-            event.currentTarget.classList.remove('alert-validate');
-        })
-    });
+inputValidate.forEach((click) => {
+    click.addEventListener('click', (event) => {
+        event.currentTarget.classList.remove('alert-validate');
+    })
+});
 
-    /*[ Show pass ]*/
-    const btnShowPass = document.querySelector('.btn-show-pass');
-    const btnShowPassi = document.querySelector('.btn-show-pass .fa-eye');
+/*[ Show pass ]*/
+const btnShowPass = document.querySelector('.btn-show-pass');
+const btnShowPassi = document.querySelector('.btn-show-pass .fa-eye');
 
-    let showPass = 0;
-    btnShowPass.addEventListener('click', (event) => {
-        if(showPass == 0) {
-            btnShowPass.nextElementSibling.setAttribute('type','text');
-            btnShowPassi.classList.remove('fa-eye');
-            btnShowPassi.classList.add('fa-eye-slash');
-            showPass = 1;
-        }
-        else {
-            btnShowPass.nextElementSibling.setAttribute('type','password');
-            btnShowPassi.classList.remove('fa-eye-slash');
-            btnShowPassi.classList.add('fa-eye');
-            showPass = 0;
-        }
+let showPass = 0;
+btnShowPass.addEventListener('click', (event) => {
+    if(showPass == 0) {
+        btnShowPass.nextElementSibling.setAttribute('type','text');
+        btnShowPassi.classList.remove('fa-eye');
+        btnShowPassi.classList.add('fa-eye-slash');
+        showPass = 1;
+    }
+    else {
+        btnShowPass.nextElementSibling.setAttribute('type','password');
+        btnShowPassi.classList.remove('fa-eye-slash');
+        btnShowPassi.classList.add('fa-eye');
+        showPass = 0;
+    }
 
-    });
+});
--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -5,18 +5,59 @@ const inputSignup = document.querySelector('#signupForm');
 const checkUsername = document.getElementById('username');
 const setPassword = document.getElementById('password');
 const passwordCheck = document.getElementById('password-check');
-const inputValidate2 = document.querySelectorAll('.signup-wrap-input.validate-input');
+const inputValidate2 = document.querySelectorAll('.wrap-input.validate-input');
 const inputArr2 = [checkUsername, setPassword, passwordCheck];
 
 inputSignup.addEventListener('submit', (event) => {
-    /*회원가입 validation참고
-    if(checkRequred([username, password])){
-        checkPasswordMatch(password1, password2)
-    }*/
     for(let i=0; i<inputArr2.length; i++) {
         if(inputArr2[i].value.trim() === ''){
             event.preventDefault();
             inputValidate2[i].classList.add('alert-validate');
         }
     }
+});
+
+inputValidate2.forEach((click) => {
+    click.addEventListener('click', (event) => {
+        event.currentTarget.classList.remove('alert-validate');
+    })
+});
+
+/*[ Show pass ]*/
+const btnShowPass = document.querySelector('.btn-show-pass');
+const btnShowPassi = document.querySelector('.btn-show-pass .fa-eye');
+const btnShowPassChk = document.querySelector('.btn-show-pass-chk');
+const btnShowPassChki = document.querySelector('.btn-show-pass-chk .fa-eye');
+
+let showPass = 0;
+btnShowPass.addEventListener('click', (event) => {
+    if(showPass == 0) {
+        btnShowPass.nextElementSibling.setAttribute('type','text');
+        btnShowPassi.classList.remove('fa-eye');
+        btnShowPassi.classList.add('fa-eye-slash');
+        showPass = 1;
+    }
+    else {
+        btnShowPass.nextElementSibling.setAttribute('type','password');
+        btnShowPassi.classList.remove('fa-eye-slash');
+        btnShowPassi.classList.add('fa-eye');
+        showPass = 0;
+    }
+
+});
+
+btnShowPassChk.addEventListener('click', (event) => {
+    if(showPass == 0) {
+        btnShowPassChk.nextElementSibling.setAttribute('type','text');
+        btnShowPassChki.classList.remove('fa-eye');
+        btnShowPassChki.classList.add('fa-eye-slash');
+        showPass = 1;
+    }
+    else {
+        btnShowPassChk.nextElementSibling.setAttribute('type','password');
+        btnShowPassChki.classList.remove('fa-eye-slash');
+        btnShowPassChki.classList.add('fa-eye');
+        showPass = 0;
+    }
+
 });
